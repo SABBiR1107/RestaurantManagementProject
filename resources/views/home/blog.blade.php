10b148d7293b511e12b33df8266f45aede9c72f1
SABBiR <ahsanursabbir@gmail.com>
2025-07-10 16:02:07 +0600

    All done

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -1,17 +1,17 @@
-    <div id="blog" class="container-fluid bg-dark text-light py-5 text-center wow fadeIn">
+<div id="blog" class="container-fluid bg-dark text-light py-5 text-center wow fadeIn">
         <h2 class="section-title py-5">Our All Foods</h2>
         
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="foods" role="tabpanel" aria-labelledby="pills-home-tab">
-                <div class="row">
+                <div class="row align-items-stretch">
                     
                 @foreach ($data as $data)
-                    <div class="col-md-4">
-                        <div class="card bg-transparent border my-3 my-md-0">
-                            <img src="food_img/{{$data->image}}"alt="photoLoading" class="rounded-0 card-img-top mg-responsive">
+                    <div class="col-md-4 d-flex">
+                        <div class="card bg-transparent border my-3 my-md-0 h-100 w-100">
+                            <img src="food_img/{{$data->image}}"alt="photoLoading" class="rounded-0 card-img img-responsive">
                             <div class="card-body">
                                 <h1 class="text-center mb-4"><a href="#" class="badge badge-primary">{{ $data->price }}</a></h1>
-                                <h4 class="pt20 pb20">{{ $data->titile }}</h4>
+                                <h4 class="pt20 pb20">{{ $data->title }}</h4>
                                 <p class="text-white">{{ $data->detail }}</p>
                             </div>
 
@@ -22,11 +22,30 @@
                             <input type="submit" value="Add to Cart" class="btn btn-info">
 
                             </form>
-                            <br><br><br>
-            
                         </div>
                     </div>
+                    
                 @endforeach
+                
             </div>
         </div>
     </div>
+    <br>
+    <br>
+
+<style>
+.card-img {
+  width: 100%;
+  aspect-ratio: 1 / 1;   /* Makes the image area a square */
+  object-fit: cover;
+  display: block;
+  height: auto;           /* Let aspect-ratio control the height */
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;       /* Make all cards the same height */
+}
+</style>
